032ec6886debfd4626f4be73a6b199749573832e
penn <penn201500@gmail.com>
2020-08-10 19:28:41 +0800

    B: filter string for choicefield

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,7 @@
 import json
 
+import django_filters
 from django.db.models import Q
-
 from .models import Todo
 from django.views.decorators.http import require_http_methods
 from .serializers.todo_serializer import TodoSerializer
@@ -9,6 +9,17 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 
+
+# class TodoTypeFilter(django_filters.FilterSet):
+#     todo_type = django_filters.CharFilter(method='filter_todo_type')
+#
+#     @staticmethod
+#     def filter_todo_type(queryset, name, value):
+#         # create a dictionary string -> integer
+#         value_map = {v: k for k, v in Todo.TYPE_CHOICE.items()}
+#         # get the integer value for the input string
+#         value = value_map[value]
+#         return queryset.filter(todo_type=value)
 
 # Create your views here.
 @csrf_exempt
@@ -21,10 +32,11 @@ def add_todo(request):
         todo.save()
         response['msg'] = 'success'
         response['error_num'] = 0
+        return JsonResponse(response)
     except Exception as e:
         response['msg'] = str(e)
         response['error_num'] = 1
-    return JsonResponse(response)
+        return JsonResponse(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 @require_http_methods(['GET'])
@@ -39,30 +51,29 @@ def show_todos(request):
         response['list'] = data_list
         response['msg'] = "success"
         response['error_num'] = 0
+        return JsonResponse(response)
     except Exception as e:
         response['msg'] = str(e)
         response['error_num'] = 1
-    return JsonResponse(response)
+        return JsonResponse(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 @csrf_exempt
-@require_http_methods(['POST'])
+@require_http_methods(['GET'])
 def search(request):
     response = {}
     data_list = []
     try:
-        # search_txt = request.query_params.get('search')
-        data = json.loads(request.body)
-        search_txt = data.get('search_txt') if data else ''
-        print(search_txt)
+        search_txt = request.GET.get('search_txt')
+        value_map = dict((v, k) for k, v in Todo.TYPE_CHOICE)
+        value = [v for k, v in value_map.items() if search_txt in k]
         if search_txt.isdigit():
-            print('here')
             todos = Todo.objects.filter(is_del=False).filter(
                 Q(id=int(search_txt)) | Q(priority=int(search_txt)) | Q(name__icontains=search_txt) | Q(
-                    comment__icontains=search_txt) | Q(todo_type=int(search_txt)))
+                    comment__icontains=search_txt) | Q(todo_type__in=value))
         else:
             todos = Todo.objects.filter(is_del=False).filter(
-                Q(name__icontains=search_txt) | Q(comment__icontains=search_txt))
+                Q(name__icontains=search_txt) | Q(comment__icontains=search_txt) | Q(todo_type__in=value))
         for ele in todos:
             data = TodoSerializer(ele).data
             data_list.append(data)
@@ -87,7 +98,8 @@ def del_todos(request):
         todos.update(is_del=True)
         response['msg'] = "delete success"
         response['error_num'] = 0
+        return JsonResponse(response)
     except Exception as e:
         response['msg'] = str(e)
         response['error_num'] = 1
-    return JsonResponse(response)
+        return JsonResponse(response, status=status.HTTP_400_BAD_REQUEST)
